56c21039c5b7a844fcb3e5c3b33e55d12724e4d6
Gustavo Ramos <gustavo.oliveira.ramos295@gmail.com>
2026-09-08 22:35:59 -0300

    corrige modelo do Gemini

--- api/consultar-ia.js
+++ api/consultar-ia.js
@@ -15,7 +15,7 @@
 
 const catalogo = require('../catalogo.json');
 
-const GEMINI_MODEL = 'gemini-2.5-flash';
+const GEMINI_MODEL = 'gemini-3.6-flash';
 const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}:generateContent`;
 
 module.exports = async (req, res) => {
